4eb3e84fa9938ee6e6a4323a00db8aa98abbe607
wrs <416551@qq.com>
2019-09-09 09:10:10 +0800

    上传工具类

---
+++ src/main/java/com/wangrunsong/string/StringUtils.java
@@ -0,0 +1,14 @@
+package com.wangrunsong.string;
+
+public class StringUtils {
+	/*
+	 *  方法功能：将字符串转换成html文本，如果遇到“\n”换行换符，则要将这一段文本使用<p></p>标签
+	 * 包起来。如果遇到“\n\r”两个在一起按上面\n处理。如果只遇到一个“\r”则替换成<br/>标签。
+	 * 使用场景：网页文本框传到后台的字符串就可能就会回车换行。
+	 * */
+	public static String toHeml(String text){
+		//实现代码
+		
+		return "";
+	}
+}
