821ed19a7e1963e847ece5f990e49dd4f17a5de8
wrs <416551@qq.com>
2019-09-09 20:22:55 +0800

    修改代码

--- src/main/java/com/wangrunsong/string/StringUtils.java
+++ src/main/java/com/wangrunsong/string/StringUtils.java
@@ -9,10 +9,17 @@ public class StringUtils {
 	 * 包起来。如果遇到“\n\r”两个在一起按上面\n处理。如果只遇到一个“\r”则替换成<br/>标签。
 	 * 使用场景：网页文本框传到后台的字符串就可能就会回车换行。
 	 * */
-	public static String toHeml(String text){
+	public static void toHeml(String str){
 		//实现代码
 		
-		return "";
+		StringBuffer buffer = new StringBuffer();
+		String[] split = str.split("(\r\n)");
+		for (String string : split) {
+			buffer.append("<p>");
+			buffer.append(string);
+			buffer.append("</p>");
+		}
+		System.out.println(buffer.toString());
 	}
 	
 	//手机号码
